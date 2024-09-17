1a12e4532e1c9b50f46d5e296f50c19618105cad
Ankita <ankita.bar1026@gmail.com>
2024-09-17 13:19:23 +0530

    calculater

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Calculater from './Calculater'
+
+function App() {
+
+  return (
+    <>
+      <Calculater/>
+    </>
+  )
+}
+
+export default App
